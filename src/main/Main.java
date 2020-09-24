39c407d7bfd53e037f4de2790b8eaba57a4f3474
Mr.Ye <1402008814@qq.com>
2020-09-24 19:38:32 +0800

    aaa

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+/**
+ * @author Mr.Ye
+ * @create 2020-09-24 19:35
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}
